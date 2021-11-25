4ea0699e50ec0d220a67a31840621d972e12e835
ShchekinIP <63049086+ShchekinIP@users.noreply.github.com>
2021-11-25 16:51:38 +0400

    Initial commit

---
+++ 01-cards-sources/app.js
@@ -0,0 +1,19 @@
+function chooseSlide(activeSlide){
+const slides = document.querySelectorAll('.slide')
+slides[activeSlide].classList.add('active')
+
+for(const slide of slides){
+    slide.addEventListener('click', () => {
+        clearActive()
+        slide.classList.add('active')
+    })
+}
+
+function clearActive(){
+    slides.forEach((slide) => {
+        slide.classList.remove('active')
+    })
+}
+}
+
+chooseSlide(2)
